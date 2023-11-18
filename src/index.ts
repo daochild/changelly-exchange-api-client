110016ffceabf57d1388ba329d4bbeb5bed32da3
daochild <paul.bolhar@gmail.com>
2023-11-18 10:51:16 +0200

    add prettier configs, remove crypto package, fix warn unsupported engine

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as crypto from 'crypto';
-import axios, {AxiosResponse} from 'axios';
-import {KeyObject} from "crypto";
-import {JsonRpcResponse} from "./typing/rpc.types";
-import { Buffer } from 'node:buffer';
+import * as crypto from "node:crypto";
+import { KeyObject } from "node:crypto";
+import { Buffer } from "node:buffer";
+import axios, { AxiosResponse } from "axios";
+import { JsonRpcResponse } from "./typing/rpc.types";
 
 import {
     ChangellyResponse,
@@ -13,63 +13,77 @@ import {
     CurrencyPair,
     CurrencyPairInfo,
     CurrencyPairWithAmount,
-    ExchangeDetails, ExchangeInfo, ExchangeInfoWithAmount,
-    ExchangeTransaction, FinishedTransaction,
+    ExchangeDetails,
+    ExchangeInfo,
+    ExchangeInfoWithAmount,
+    ExchangeTransaction,
+    FinishedTransaction,
     FixedExchangeTransaction,
-    SearchTransactions, TransactionStatus, ValidationResult
+    SearchTransactions,
+    TransactionStatus,
+    ValidationResult,
 } from "./typing/changelly.types.js";
 
 export class Changelly {
     private readonly apiKey: string;
     private readonly privKey: KeyObject;
 
-    public static readonly version: string = '2.0';
-    public static readonly url: string = 'https://api.changelly.com/v2';
+    public static readonly version: string = "2.0";
+    public static readonly url: string = "https://api.changelly.com/v2";
 
     constructor(apiKey: string, privKey: string) {
         this.apiKey = apiKey;
         this.privKey = crypto.createPrivateKey({
             key: privKey,
-            format: 'der',
-            type: 'pkcs8'
+            format: "der",
+            type: "pkcs8",
         });
     }
 
     private _id(): string {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            const r = (Math.random() * 16) | 0;
-            const v = c === 'x' ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        });
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+            /[xy]/g,
+            function (c) {
+                const r = (Math.random() * 16) | 0;
+                const v = c === "x" ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            },
+        );
     }
 
     private _sign(message: object): string {
         // @ts-ignore
-        return crypto.sign('sha256', Buffer.from(JSON.stringify(message)), {
-            // @ts-ignore
-            key: this.privKey,
-            type: 'pkcs8',
-            format: 'der'
-        }).toString('base64');
+        return crypto
+            .sign("sha256", Buffer.from(JSON.stringify(message)), {
+                // @ts-ignore
+                key: this.privKey,
+                type: "pkcs8",
+                format: "der",
+            })
+            .toString("base64");
     }
 
-    private async _request(method: string, params: any): Promise<any|JsonRpcResponse> {
+    private async _request(
+        method: string,
+        params: any,
+    ): Promise<any | JsonRpcResponse> {
         const message = {
-            "jsonrpc": "2.0",
-            "id": this._id(),
-            "method": method,
-            "params": params
+            jsonrpc: "2.0",
+            id: this._id(),
+            method: method,
+            params: params,
         };
         const axiosConfig = {
             headers: {
-                'Content-Type': 'application/json',
-                'X-Api-Key': this.apiKey,
-                'X-Api-Signature': this._sign(message)
+                "Content-Type": "application/json",
+                "X-Api-Key": this.apiKey,
+                "X-Api-Signature": this._sign(message),
             },
-        }
+        };
 
         return new Promise((resolve, reject) => {
-            axios.post(Changelly.url, message, axiosConfig)
+            axios
+                .post(Changelly.url, message, axiosConfig)
                 .then((response: AxiosResponse) => {
                     if (response.data.error) {
                         reject(response.data.error);
@@ -88,97 +102,108 @@ export class Changelly {
     }
 
     public static agreeWithKycMessage = function (): string {
-        return 'Exchange services provided by Changelly. By clicking “Accept”, I acknowledge and understand that my transaction may trigger AML/KYC verification according to Changelly AML/KYC”';
-    }
+        return "Exchange services provided by Changelly. By clicking “Accept”, I acknowledge and understand that my transaction may trigger AML/KYC verification according to Changelly AML/KYC”";
+    };
 
     public static kycRequestMessage = function (): string {
-        return 'Your transaction has been frozen. Please contact with Changelly security team at security@changelly.com in order to pass the KYC procedure.';
-    }
+        return "Your transaction has been frozen. Please contact with Changelly security team at security@changelly.com in order to pass the KYC procedure.";
+    };
 
     public async createTransaction(
-        params: CreateTransactionDetails|CreateTransactionWithRefundDetails
+        params: CreateTransactionDetails | CreateTransactionWithRefundDetails,
     ): Promise<ChangellyResponse<ExchangeTransaction>> {
-        return this._request('createTransaction', params);
+        return this._request("createTransaction", params);
     }
 
     public async createFixTransaction(
-        params: CreateFixTransactionDetails
+        params: CreateFixTransactionDetails,
     ): Promise<ChangellyResponse<FixedExchangeTransaction>> {
-        return this._request('createFixTransaction', params);
+        return this._request("createFixTransaction", params);
     }
 
     public async getCurrencies(): Promise<ChangellyResponse<string[]>> {
-        return this._request('getCurrencies', {});
+        return this._request("getCurrencies", {});
     }
 
     public async getCurrenciesFull(): Promise<ChangellyResponse<Currency[]>> {
-        return this._request('getCurrenciesFull', {});
+        return this._request("getCurrenciesFull", {});
     }
 
-    public async getPairsParams(params: CurrencyPair[]): Promise<ChangellyResponse<CurrencyPairInfo[]>> {
+    public async getPairsParams(
+        params: CurrencyPair[],
+    ): Promise<ChangellyResponse<CurrencyPairInfo[]>> {
         if (params.length === 0) {
             throw new Error("Pairs empty");
         }
 
-        return this._request('getPairsParams', params);
+        return this._request("getPairsParams", params);
     }
 
     public async getMinAmount(
         from: string,
-        to: string
+        to: string,
     ): Promise<ChangellyResponse<string>> {
         const params = {
             from: from,
             to: to,
         };
 
-        console.warn("getMinAmount: method DEPRECATED")
+        console.warn("getMinAmount: method DEPRECATED");
 
-        return this._request('getMinAmount', params);
+        return this._request("getMinAmount", params);
     }
 
     public async getExchangeAmount(
-        params: CurrencyPairWithAmount|CurrencyPairWithAmount[]
+        params: CurrencyPairWithAmount | CurrencyPairWithAmount[],
     ): Promise<ChangellyResponse<ExchangeDetails[]>> {
-        return this._request('getExchangeAmount', params);
+        return this._request("getExchangeAmount", params);
     }
 
     public async getTransactions(
-        params: SearchTransactions
+        params: SearchTransactions,
     ): Promise<ChangellyResponse<FinishedTransaction[]>> {
-        return this._request('getTransactions', params);
+        return this._request("getTransactions", params);
     }
 
-    public async getStatus(id: string): Promise<ChangellyResponse<TransactionStatus>> {
+    public async getStatus(
+        id: string,
+    ): Promise<ChangellyResponse<TransactionStatus>> {
         const params = {
             id: id,
         };
 
-        return this._request('getStatus', params);
+        return this._request("getStatus", params);
     }
 
-    public async getFixRate(params: CurrencyPair[]): Promise<ChangellyResponse<ExchangeInfo[]>> {
+    public async getFixRate(
+        params: CurrencyPair[],
+    ): Promise<ChangellyResponse<ExchangeInfo[]>> {
         if (params.length === 0) {
             throw new Error("Pairs empty");
         }
 
-        return this._request('getFixRate', params);
+        return this._request("getFixRate", params);
     }
 
-    public async getFixRateForAmount(params: CurrencyPairWithAmount[]): Promise<ChangellyResponse<ExchangeInfoWithAmount[]>> {
+    public async getFixRateForAmount(
+        params: CurrencyPairWithAmount[],
+    ): Promise<ChangellyResponse<ExchangeInfoWithAmount[]>> {
         if (params.length === 0) {
             throw new Error("Pairs empty");
         }
 
-        return this._request('getFixRateForAmount', params);
+        return this._request("getFixRateForAmount", params);
     }
 
-    public async validateAddress(currency: string, address: string): Promise<ChangellyResponse<ValidationResult>> {
+    public async validateAddress(
+        currency: string,
+        address: string,
+    ): Promise<ChangellyResponse<ValidationResult>> {
         const params = {
             currency: currency,
             address: address,
         };
 
-        return this._request('validateAddress', params);
+        return this._request("validateAddress", params);
     }
 }
